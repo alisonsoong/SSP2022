8039ec23400bfbc4c2ba14fb4f1f436d2652d78f
Alison <alisonsoong@gmail.com>
2022-07-15 22:50:28 -0600

    added cleaned up OD class

--- odlib.py
+++ odlib.py
@@ -373,3 +373,242 @@ class ODElements:
                 floats: a,e,i,o,v,w,T,M
         """
         return self.a, self.e, self.i, self.o, self.v, self.w, self.T, self.M
+    
+    
+    
+# OD class
+class OD:
+    '''Class that performs all orbital determination calculations'''
+    
+    def __init__(self):
+        """ Initializes OD class
+            Args:
+                None
+            Returns:
+                None
+        """
+        # constants
+        self.k = 0.0172020989484 #Gaussian gravitational constant  
+        self.cAU = 173.144643267 #speed of light in au/(mean solar)day  
+        self.mu = 1
+        self.eps = np.radians(23.4374) #Earth's obliquity
+        
+    def genElements(self, pos:list, vel:list, time:float, update:bool=True):
+        """ Calculates and returns the orbital elements given position, velocity, time
+            Args:
+                pos (list): the position vector
+                vel (list): the velocity vector
+                time (float): the time in Julian days
+                update (bool): if True keeps the newly calculated orbital elements
+            Returns:
+                floats: the orbital elements; a,e,i,o,v,w,T,M
+        """
+        if update:
+            self.pos,self.vel,self.time=pos,vel,time
+            self.od=ODElements(self.pos,self.vel,self.time)
+            self.a,self.e,self.i,self.o,self.v,self.w,self.T,self.M = self.od.getElements()
+            return self.getElements()
+        else:
+            od=ODElements(pos,vel,time)
+            return od.getElements()
+    
+    def getElements(self):
+        """ Returns the orbital elements (already calculated)
+            Args:
+                rad (bool): True if return in radians
+            Returns:
+                floats: a,e,i,o,v,w,T,M
+        """
+        return self.a, self.e, self.i, self.o, self.v, self.w, self.T, self.M
+    
+    def printODElementsErr(self, res:str):
+        """ Prints the OD elements and compares them to results (expected, calculated, % error)
+            Args:
+                res (list): results
+            Returns:
+                None
+        """
+        self.od.printError(res)
+    
+    def getT(self)->float:
+        """ Returns the time of perihelion
+            Args:
+                None
+            Returns:
+                float: time of perihelion
+        """
+        return self.T # time of perihelion
+    
+    def SEL(self, taus:list, sunR2:list, rhohat2:list, coefD:list):
+        """ Scalar Equation of Lagrange to calculate the roots (r) and rhos corresponding to each r
+            Args:
+                taus (list): a list of floats of taus [T1,T3,T]
+                sunR2 (list): a list representing the sun vector R2
+                rhohat2 (list): a list representing the rhohat2 vector
+                coefD (list): a list of D coefficients [D0,D21,D22,D23]
+            Returns:
+                lists: roots (r's), rhos
+        """
+        T1,T3,T=taus[0],taus[1],taus[2]
+        D0,D21,D22,D23=coefD[0],coefD[1],coefD[2],coefD[3]
+        A1=T3/T
+        B1=A1/6*(T**2-T3**2)
+        A3=-T1/T
+        B3=A3/6*(T**2-T1**2)
+        A=(A1*D21-D22+A3*D23)/(-D0)
+        B=(B1*D21+B3*D23)/(-D0)
+        
+        E=-2*(dot(rhohat2, sunR2))
+        F=dot(sunR2,sunR2)
+        
+        a=-(A**2+A*E+F)
+        b=-self.od.mu*(2*A*B+B*E)
+        c=-self.od.mu**2*B**2
+        
+        coef=[c,0,0,b,0,0,a,0,1]#[1,0,a,0,0,b,0,0,c]
+        res=poly.polyroots(coef)
+
+        roots=[]
+        for val in res: 
+            if np.isreal(val) and np.real(val)>0: roots.append(np.real(val))
+
+        rhos=[A+B/roots[i]**3 for i in range(len(roots))]
+        return roots,rhos
+    
+    def ephemeris(self, time:float, date:str):
+        """ Calculates RA and Dec given time and date, using previously calculated orbital elements
+            Args:
+                time (float): time to determine ephemeris for in Julian Days
+                date (str): date to determine ephemeris for
+            Returns:
+                floats: ra, dec
+        """
+        n=self.k*math.sqrt(self.mu/(self.a**3))
+        M=n*(time-self.T)
+        E=newton(lambda E:M - E + self.e*np.sin(E), lambda E: -1+self.e*np.cos(E), M, 1e-14)
+        
+        pos=np.array([self.a*math.cos(E)-self.a*self.e, self.a*math.sqrt(1-self.e**2)*math.sin(E), 0])
+        
+        # the four rotations
+        pos=rotZX(pos,np.deg2rad(self.w),np.deg2rad(self.i))
+        pos=rotZX(pos,np.deg2rad(self.o),self.eps)
+        
+        rho=pos+self.getSunPos(date)
+        rhohat=rho/getMag(rho)
+        
+        dec=math.asin(rhohat[2])
+        cra=rhohat[0]/math.cos(dec)
+        sra=rhohat[1]/math.cos(dec)
+
+        ra=getAngle(sra,cra)
+        
+        dec=np.rad2deg(dec)
+        ra=np.rad2deg(ra)
+        
+        dec=DECdecimalToDMS(dec)
+        ra=RAdecimalToHMS(ra)
+        
+        return ra,dec
+        
+    def getSunPos(self, date:str)->list:
+        """ Gets the vector from the Earth to the Sun given the date
+            Args:
+                date (str): the date to use
+            Returns:
+                list: sun vector R
+        """
+        info=np.loadtxt("/home/soonali/Documents/SunPos.txt",dtype=str,delimiter=",")
+        #print(info)
+        timestamps=info[:,1]
+        for i in range(len(timestamps)):
+            if date in timestamps[i]: 
+                line = i
+                break
+       
+        stuff=info[line,:]
+        x,y,z=float(stuff[2]),float(stuff[3]),float(stuff[4])
+        return np.array([x,y,z])
+    
+    def getSunMag(self,date:str)->float:
+        """ Gets the magnitude of the position vector from sun to earth
+            Args:
+                date (str): the date to use
+            Returns:
+                float: the magnitude of vector R
+        """
+        return getMag(getSunPos(date))
+        
+    def fg(self, tau:float, r2:list, r2dot:list, flag:bool):
+        """ Gets the f and g values given one time
+            Args:
+                tau (float): the time in Julian Days
+                r2 (list): the position vector 2
+                r2dot (list): the velocity vector 2
+                flag (bool): True if fourth term of Taylor Series expansion is needed
+            Returns:
+                floats: f, g
+        """
+        u=self.mu/getMag(r2)**3
+        z=dot(r2,r2dot)/(dot(r2,r2))
+        q=dot(r2dot,r2dot)/(dot(r2,r2))-u
+        
+        f=1-1/2*u*tau**2+1/2*u*z*tau**3
+        g=tau-1/6*u*tau**3
+        if flag:
+            f+=1/24*(3*u*q-15*u*z**2+u**2)*tau**4
+            g+=1/4*u*z*tau**4
+        
+        return f, g
+        
+    def getFGVals(self, tau1:float, tau3:float, r2:list, r2dot:list, flag1:bool, flag3:bool):
+        """ Gets all f and g values
+            Args:
+                tau1 (float): the time in Julian Days for observation 1
+                tau3 (float): the time in Julian days for observation 3
+                r2 (list): the position vector 2
+                r2dot (list): the velocity vector 2
+                flag1 (bool): True if fourth term of Taylor Series expansion is needed for observation1
+                flag2 (bool): True if fourth term of Taylor Series expansion is needed for observation2
+            Returns:
+                lists: [f1,f3], [g1,g3]
+        """
+        f1,g1=self.fg(tau1,r2,r2dot,flag1)
+        f3,g3=self.fg(tau3,r2,r2dot,flag3)
+        return [f1,f3], [g1,g3]
+    
+    def getDCoef(self, ra:list, dec:list, R1:list, R2:list, R3:list)->list:
+        """ Gets the D coefficients given the ra and dec for three observations (in radians)
+            Args:
+                ra (list): the right ascensions for three observations (radians)
+                dec (list): the declinations for three observations (radians)
+                R1 (list): the sun vector for observation 1
+                R2 (list): the sun vector for observation 2
+                R3 (list): the sun vector for observation 3
+            Returns:
+                list: [D0,D11,D12,D13,D21,D22,D23,D31,D32,D33]
+        """
+        ra1,ra2,ra3=ra[0],ra[1],ra[2]
+        dec1,dec2,dec3=dec[0],dec[1],dec[2]
+        
+        rhohat1=np.array([np.cos(ra1)*np.cos(dec1), np.sin(ra1)*np.cos(dec1), np.sin(dec1)])
+        rhohat2=np.array([np.cos(ra2)*np.cos(dec2), np.sin(ra2)*np.cos(dec2), np.sin(dec2)])
+        rhohat3=np.array([np.cos(ra3)*np.cos(dec3), np.sin(ra3)*np.cos(dec3), np.sin(dec3)])
+        
+        D0=dot(rhohat1, cross(rhohat2,rhohat3))
+        D11=dot(cross(R1, rhohat2),rhohat3)
+        D12=dot(cross(R2, rhohat2),rhohat3)
+        D13=dot(cross(R3, rhohat2),rhohat3)
+        
+        D21=dot(cross(rhohat1,R1), rhohat3)
+        D22=dot(cross(rhohat1,R2), rhohat3)
+        D23=dot(cross(rhohat1,R3), rhohat3)
+        
+        D31=dot(rhohat1, cross(rhohat2,R1))
+        D32=dot(rhohat1, cross(rhohat2,R2))
+        D33=dot(rhohat1, cross(rhohat2,R3))
+     
+        return [D0,D11,D12,D13,D21,D22,D23,D31,D32,D33]
+        
+        
+
+    
